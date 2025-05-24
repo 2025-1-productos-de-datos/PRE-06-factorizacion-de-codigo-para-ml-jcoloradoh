2460bfb8bf730bb06375f3e6857a11699258cc69
jcoloradoh <jcoloradoh@unal.edu.co>
2025-05-24 08:00:26 -0500

    PRE_06

---
+++ homework/__main__.py
@@ -0,0 +1,9 @@
+# __main__.py
+"""Entry point for the homework package."""
+
+# python -m homework --model elasticnet --l1_ratio 0.1 --alpha 0.9
+
+from .src.main import main
+
+if __name__ == "__main__":
+    main()
